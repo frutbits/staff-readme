2f7bdb0f6fc2ac58df385b2d17aa858f1e2eb137
ZenShibata <zenshibata@outlook.com>
2022-05-31 08:34:29 +0700

    feat(config): remove default config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import core from "@actions/core";
 
-export const COMMIT_MSG = core.getInput("COMMIT_MSG") || "chore(readme): Update staff list";
+export const COMMIT_MSG = core.getInput("COMMIT_MSG");
