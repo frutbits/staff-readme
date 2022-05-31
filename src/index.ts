8097125499f57269ad35aab834cd9ec6137d87de
ZenShibata <zenshibata@outlook.com>
2022-05-31 15:02:21 +0700

    fix: better readme formatting

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,9 @@ const startComment = "<!--START_SECTION:administrator_list-->";
 const endComment = "<!--END_SECTION:administrator_list-->";
 
 Toolkit.run(async tools => {
+    // Define root directory
     const workingDirectory = process.env.WORKING_DIRECTORY ?? ".";
+    // Fetch data from /membership
     tools.log.debug("GET /membership");
     const response = await request("https://api.zhycorp.org/membership").catch((e: Error) => e);
     if (response instanceof Error) {
@@ -18,42 +20,45 @@ Toolkit.run(async tools => {
     if (response.statusCode !== 200) {
         return tools.exit.failure(`Received non-200 response code: ${response.statusCode}`);
     }
+
     let data: APIResponse;
     try {
-        data = await response.body.json() as APIResponse;
+        data = await response.body.json();
     } catch {
         return tools.exit.failure("Failed to parse membership data");
     }
+
     tools.log.debug(`Found ${data.administrator.length} administrator`);
+
     if (data.administrator.length === 0) {
-        return tools.exit.failure("Administrator is empty, aborting process");
+        return tools.exit.success("Administrator is empty, aborting update");
     }
 
-    const readmeContent = readFileSync(`${workingDirectory}/README.md`, "utf-8").split("\n");
-    let startIndex = readmeContent.findIndex(c => c.trim() === startComment);
+    let readmeContent = readFileSync(`${workingDirectory}/README.md`, "utf-8").split("\n");
+
+    // Look for staff list section
+    const startIndex = readmeContent.findIndex(c => c.trim() === startComment);
     if (startIndex === -1) {
         return tools.exit.failure(`Couldn't find the "${startComment}" comment`);
     }
 
-    const endIndex = readmeContent.findIndex(c => c.trim() === endComment);
+    let endIndex = readmeContent.findIndex(c => c.trim() === endComment);
     if (endIndex === -1) {
         return tools.exit.failure(`Couldn't find the "${endComment}" comment`);
     }
 
-    const content = parseTable(data.administrator)
-        .map(x => `<tr>${x.join("\n")}</tr>`);
-    content.unshift("<table>");
-    content.push("</table>");
+    const content = ["<table>", ...parseTable(data.administrator)
+        .map(x => `<tr>${x.join("")}</tr>`), "</table>"];
 
-    if (startIndex !== -1 && endIndex === -1) {
-        startIndex++;
-        content.forEach((line, i) => readmeContent.splice(startIndex + i, 0, line));
-        readmeContent.splice(
-            startIndex + content.length,
-            0,
-            "<!--END_SECTION:administrator_list>"
-        );
+    const existingData = readmeContent.slice(startIndex + 1, endIndex);
+    readmeContent = readmeContent.filter(x => !existingData.includes(x));
+    endIndex = readmeContent.findIndex(c => c.trim() === endComment);
 
+    const matched = existingData.map(x => content.find(y => x === y) !== undefined);
+    // Need to be updated
+    if (matched.some(x => !x)) {
+        tools.log.info("Found diff data. Updating..");
+        readmeContent.splice(startIndex + 1, 0, ...content);
         writeFileSync(`${workingDirectory}/README.md`, readmeContent.join("\n"));
         try {
             await commitFile();
@@ -61,45 +66,9 @@ Toolkit.run(async tools => {
             tools.log.debug("Something went wrong");
             return tools.exit.failure(err as string);
         }
-        return tools.exit.success("Updated README");
+        tools.exit.success("Pushed to remote repository");
     }
-
-    const oldContent = readmeContent.slice(startIndex + 1, endIndex).join("\n");
-    const newContent = content.join("\n");
-
-    if (oldContent.trim() === newContent.trim()) {
-        return tools.exit.success("No changes detected");
-    }
-
-    startIndex++;
-    const staffSection = readmeContent.slice(startIndex, endIndex);
-    if (staffSection.length) {
-        let count = 0;
-        staffSection.some((line, idx) => {
-            if (!content[count]) {
-                return true;
-            }
-            if (line !== "") {
-                readmeContent[startIndex + idx] = content[count];
-                count++;
-            }
-        });
-        tools.log.success("Overwrited readme");
-    } else {
-        content.some((line, index) => {
-            if (!line) return true;
-            readmeContent.splice(startIndex + index, 0, line);
-        });
-        tools.log.success("Updated README");
-    }
-    writeFileSync(`${workingDirectory}/README.md`, readmeContent.join("\n"));
-    try {
-        await commitFile();
-    } catch (err) {
-        tools.log.debug("Something went wrong");
-        return tools.exit.failure(err as string);
-    }
-    tools.exit.success("Pushed to remote repository");
+    tools.exit.success("Up-to-date already");
 }, {
     event: ["schedule", "workflow_dispatch"]
 }).catch(e => console.error(e));
