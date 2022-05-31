18381ddb4997dc3c7e4882f0e431048428f93c7e
ZenShibata <zenshibata@outlook.com>
2022-05-31 09:17:31 +0700

    fix(commitFile): add dot instead of file

--- src/utils/commitFile.ts
+++ src/utils/commitFile.ts
@@ -8,7 +8,7 @@ export async function commitFile(): Promise<any> {
         "github-actions[bot]@users.noreply.github.com"
     ]);
     await exec("git", ["config", "--global", "user.name", "github-actions[bot]"]);
-    await exec("git", ["add", "README.md"]);
+    await exec("git", ["add", "."]);
     await exec("git", ["commit", "-m", "chore(readme): Update staff list"]);
     await exec("git", ["push"]);
 }
